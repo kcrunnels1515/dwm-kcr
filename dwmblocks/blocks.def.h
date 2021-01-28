7c74a86112d65ee32d6701230f2bd3a05efbf645
KCRunnels15 <kcrunnels15@gmail.com>
2021-01-28 17:19:36 -0500

    Added battery for dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,6 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
 	{ "  ", "iw wlp3s0 info | grep ssid | sed 's/	ssid//'", 60, 0},
+/*	{ "Bat: ",	"acpi | awk '{print $4}' | sed 's/,/	/'",	15,	0}, */
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
