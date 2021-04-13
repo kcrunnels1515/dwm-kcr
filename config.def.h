536a5a42f658df7b99f0aca58633a3164553741c
KCRunnels15 <kcrunnels15@gmail.com>
2021-04-13 16:42:50 -0400

    Removed rounded corners picom

--- config.def.h
+++ config.def.h
@@ -40,7 +40,7 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"nitrogen", "--restore", NULL,
-	"picom", "--config", "/usr/local/share/dwm/picom.conf", NULL,
+	"picom", NULL,
 	"dunst", NULL,
 	"/usr/lib/polkit-kde-authentication-agent-1", NULL,
 	"numlockx", "on", NULL,
