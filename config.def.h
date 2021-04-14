339220831dadcf575cc16d5b462ef5903d9ec748
KCRunnels15 <kcrunnels15@gmail.com>
2021-04-14 13:32:35 -0400

    Changed unselected monitor color to purple

--- config.def.h
+++ config.def.h
@@ -26,6 +26,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_purple[]	    = "#4c2172";
 static const char col_cyan[]        = "#005577";
 static const char col_green[]	    = "#465e25";
 /* static const char col_
@@ -34,7 +35,7 @@ static const char col_ */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeInv] = { col_gray1, col_gray3, col_gray2 },
+	[SchemeInv] = { col_gray3, col_purple, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_green,  col_green  },
 };
 
