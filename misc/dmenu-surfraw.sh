c2fd4d61cde38eeaf26802e001a3a15324e1600b
KCRunnels15 <kcrunnels15@gmail.com>
2021-04-26 00:06:47 -0400

    Added history into search script

--- misc/dmenu-surfraw.sh
+++ misc/dmenu-surfraw.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-#  ____ _____ 
+#  ____ _____
 # |  _ \_   _|  Derek Taylor (DistroTube)
 # | | | || |    http://www.youtube.com/c/DistroTube
-# | |_| || |    http://www.gitlab.com/dwt1/ 
-# |____/ |_| 
+# | |_| || |    http://www.gitlab.com/dwt1/
+# |____/ |_|
 #
 # Dmenu script for launching surfaw, a command line search utility.
 
 cmd="dmenu -l 10 -i"
+histfile="$HOME/.cache/hist_cache"
 query=$@
+
+test -f $histfile || touch $histfile
 
 while [ -z "$engine" ]; do
 engine=$(sr -elvi | gawk '{if (NR!=1) { print $1 }}' | $cmd -p "Search engine?") || exit
 done
 
 while [ -z "$query" ]; do
-query=$(echo "" | $cmd -p "Searching $engine") || exit
+query=$(cat $histfile | $cmd -p "Searching $engine") || exit
 done
 
 sr -g "$engine" $query
+echo $query >> $histfile
